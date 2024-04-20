9e072380ffdea779ce83e3592be37cb5564d6bf4
samatbelentbay <belentbais03@mail.ru>
2024-04-20 23:59:21 +0500

    reverse sort

--- src/main.rs
+++ src/main.rs
@@ -22,4 +22,24 @@ fn main() {
     let mut strings = vec!["banana", "apple", "cherry", "date", "elderberry"];
     quick_sort(&mut strings, &|a, b| a.cmp(b));
     println!("Quick Sort: {:?}", strings);
+
+    let mut nums = vec![4, 2, 5, 1, 3];
+    quick_sort(&mut nums, &|a, b| b.cmp(a));
+    println!("Quick Sort (Reverse): {:?}", nums);
+
+    let mut nums = vec![4, 2, 5, 1, 3];
+    selection_sort(&mut nums, &|a, b| b.cmp(a));
+    println!("Selection Sort (Reverse): {:?}", nums);
+
+    let mut nums = vec![4, 2, 5, 1, 3];
+    insertion_sort(&mut nums, &|a, b| b.cmp(a));
+    println!("Insertion Sort (Reverse): {:?}", nums);
+
+    let mut nums = vec![9,8,6,4,2,62,6];
+    merge_sort(&mut nums, &|a, b| b.cmp(a));
+    println!("Merge Sort (Reverse): {:?}", nums);
+
+    let mut strings = vec!["banana", "apple", "cherry", "date", "elderberry"];
+    quick_sort(&mut strings, &|a, b| b.cmp(a));
+    println!("Quick Sort (Reverse): {:?}", strings);
 }
